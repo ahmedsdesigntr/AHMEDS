eec52de55f35c03deddb030969f954958f068f8b
ahmed_watfe <aliwate1209@gmail.com>
2026-04-15 09:46:47 +0300

    Add basic random number generator program

---
+++ random num generator.cpp
@@ -0,0 +1,9 @@
+include<iostream>
+using namespace std;
+rand() % 10 + 1; // Generates a random number between 1 and 10
+int main() {
+    int randomNumber = rand() % 10 + 1; // Generates a random number between 1 and 10
+    cout << "Random Number
+: " << randomNumber << endl;
+    return 0;
+}
